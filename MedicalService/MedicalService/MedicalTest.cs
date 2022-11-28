9687361424ef5385bd1a268ebf9fc386f99760a0
QA36 <QA36@DESKTOP-AESHFBQ>
2022-11-28 20:57:14 +0100

    Init

---
+++ MedicalService/MedicalService/MedicalTest.cs
@@ -0,0 +1,40 @@
+using MedicalService.Driver;
+using MedicalService.Page;
+
+namespace MedicalService
+{
+    public class Tests
+    {
+        LoginPage loginPage;
+        MedicalPage medicalPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+            medicalPage = new MedicalPage();
+        }
+        [TearDown]
+
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+        }
+
+        [Test]
+        public void TC01_MakeAppointment_ShouldAppointmentBeCompleted()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("John Doe", "ThisIsNotAPassword");
+            medicalPage.SelectFacility("Tokyo CURA Healthcare Center");
+            medicalPage.CheckBox.Click();
+            medicalPage.MedicaId.Click();
+            medicalPage.Date.SendKeys("25/12/2022");
+            medicalPage.TextComment.SendKeys("Zakazano");
+            medicalPage.ButtonBookAppoiment.Submit();
+
+            Assert.That("Appointment Confirmation", Is.EqualTo(medicalPage.Confirm.Text));
+        }
+    }
+}
---
+++ MedicalService/MedicalService/Page/MedicalPage.cs
@@ -0,0 +1,24 @@
+﻿using MedicalService.Driver;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace MedicalService.Page
+{
+    public class MedicalPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+
+        public void SelectFacility(string text)
+        {
+            SelectElement element = new SelectElement(driver.FindElement(By.Id("combo_facility")));
+            element.SelectByText(text);
+        }
+
+        public IWebElement CheckBox => driver.FindElement(By.Id("chk_hospotal_readmission"));
+        public IWebElement MedicaId => driver.FindElement(By.Id("radio_program_medicaid"));
+        public IWebElement Date => driver.FindElement(By.Id("txt_visit_date"));
+        public IWebElement TextComment => driver.FindElement(By.Id("txt_comment"));
+        public IWebElement ButtonBookAppoiment => driver.FindElement(By.Id("btn-book-appointment"));
+        public IWebElement Confirm => driver.FindElement(By.CssSelector(".text-center h2"));
+    }
+}
